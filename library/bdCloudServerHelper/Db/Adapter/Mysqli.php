xfrocks/bdCloudServerHelper

commit fe5581557f46d4166df48901e34c964c8bb3c456

added support for master/slave db switching using session information

library/bdCloudServerHelper/Db/Adapter/Mysqli.php
@@ -4,6 +4,57 @@
 // $config['db']['adapterNamespace'] = 'bdCloudServerHelper_Db_Adapter';
 class bdCloudServerHelper_Db_Adapter_Mysqli extends Zend_Db_Adapter_Mysqli
 {
+    protected function _connect()
+    {
+        if ($this->_connection) {
+            return;
+        }
+
+        $hostsArray = null;
+        $masterHost = null;
+
+        if ($this->_config['host'] instanceof Zend_Config) {
+            /** @noinspection PhpUndefinedMethodInspection */
+            $hostsArray = $this->_config['host']->toArray();
+        }
+        if ($hostsArray !== null) {
+            if (!empty($hostsArray['master'])) {
+                $masterHost = $hostsArray['master'];
+                unset($hostsArray['master']);
+            } else {
+                $hostArrayKeys = array_keys($hostsArray);
+                $masterHostKey = reset($hostArrayKeys);
+                $masterHost = $hostsArray[$masterHostKey];
+                unset($hostsArray[$masterHostKey]);
+            }
+        }
+        if ($masterHost !== null) {
+            $this->_config['host'] = $masterHost;
+        }
+
+        if ($hostsArray !== null
+            && XenForo_Application::isRegistered('session')
+            && XenForo_Application::getSession()->get('user_id') == 0
+        ) {
+            // session has been registered and reports some guest is browsing the site
+            // this means some backend cache has been enabled and db is now being lazy-loaded
+            // switch to use a random slave db host
+            if (bdCloudServerHelper_Listener::isReadOnly()) {
+                // read only mode: also consider the master host for random pick
+                // because user cannot login anyway, we couldn't afford not using the master one
+                $hostsArray[] = $masterHost;
+            }
+            shuffle($hostsArray);
+            $this->_config['host'] = reset($hostsArray);
+        }
+
+        parent::_connect();
+
+        if (!empty($this->_connection)) {
+            $this->_connection->query('SET @@session.sql_mode=\'STRICT_ALL_TABLES\'');
+        }
+    }
+
     public function query($sql, $bind = array())
     {
         if (bdCloudServerHelper_Listener::isReadOnly()

commit fae9b8ad0e4c56377cc74d02d1c56977779653f1

fixed bug caused by fe55815

library/bdCloudServerHelper/Db/Adapter/Mysqli.php
@@ -32,7 +32,7 @@ class bdCloudServerHelper_Db_Adapter_Mysqli extends Zend_Db_Adapter_Mysqli
             $this->_config['host'] = $masterHost;
         }
 
-        if ($hostsArray !== null
+        if (!empty($hostsArray)
             && XenForo_Application::isRegistered('session')
             && XenForo_Application::getSession()->get('user_id') == 0
         ) {

library/bdCloudServerHelper/addon-bdCloudServerHelper.xml
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<addon addon_id="bdCloudServerHelper" title="[bd] Cloud Server Helper" version_string="1.5.5" version_id="1050500" url="" install_callback_class="" install_callback_method="" uninstall_callback_class="" uninstall_callback_method="">
+<addon addon_id="bdCloudServerHelper" title="[bd] Cloud Server Helper" version_string="1.5.5" version_id="1050501" url="" install_callback_class="" install_callback_method="" uninstall_callback_class="" uninstall_callback_method="">
   <admin_navigation/>
   <admin_permissions/>
   <admin_style_properties/>