52db24deb5fc01dcf948ba7aba104e2d298535c0
Dao Hoang Son <daohoangson@gmail.com>
2017-01-02 23:02:49 +0700

    Make up fake session data for read only mode

--- library/bdCloudServerHelper/XenForo/Session.php
+++ library/bdCloudServerHelper/XenForo/Session.php
@@ -2,16 +2,33 @@
 
 class bdCloudServerHelper_XenForo_Session extends XFCP_bdCloudServerHelper_XenForo_Session
 {
-    public function get($key)
-    {
-        if (bdCloudServerHelper_Listener::isReadOnly()
-            && $this->_config['table'] === 'xf_session'
-            && $key === 'user_id'
-        ) {
-            return 0;
-        }
+    const SESSION_ID_READ_ONLY = 'read_only';
 
-        return parent::get($key);
+    protected function _setup($sessionId = '', $ipAddress = false, array $defaultSession = null)
+    {
+        parent::_setup(self::SESSION_ID_READ_ONLY, $ipAddress, $defaultSession);
     }
 
+    public function getSessionFromSource($sessionId)
+    {
+        if ($sessionId === self::SESSION_ID_READ_ONLY) {
+            return array(
+                'sessionCsrf' => XenForo_Application::generateRandomString(16),
+                'sessionStart' => XenForo_Application::$time,
+                'user_id' => 0,
+            );
+        }
+
+        return false;
+    }
+
+    public function deleteSessionFromSource($sessionId)
+    {
+        // intentionally left empty
+    }
+
+    public function saveSessionToSource($sessionId, $isUpdate)
+    {
+        // intentionally left empty
+    }
 }
