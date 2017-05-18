ace8764d713d1141405d1dcdeff09aaedc7e26fa
Dao Hoang Son <daohoangson@gmail.com>
2017-05-19 00:38:22 +0700

    Optimize sync redis counter to db with batch update

--- library/bdCloudServerHelper/Helper/RedisValueSync.php
+++ library/bdCloudServerHelper/Helper/RedisValueSync.php
@@ -9,12 +9,8 @@ class bdCloudServerHelper_Helper_RedisValueSync
     {
         self::_start(
             'attachment_view',
-            function (Zend_Db_Adapter_Abstract $db, $varName, $value) {
-                $db->query('
-                    UPDATE xf_attachment
-                    SET view_count = view_count + ?
-                    WHERE attachment_id = ?
-                ', array($value, $varName));
+            function (Zend_Db_Adapter_Abstract $db, $values) {
+                self::_increaseDbColumnByKey($db, 'xf_attachment', 'view_count', 'attachment_id', $values);
             }
         );
     }
@@ -23,12 +19,8 @@ class bdCloudServerHelper_Helper_RedisValueSync
     {
         self::_start(
             'bdad_click',
-            function (Zend_Db_Adapter_Abstract $db, $varName, $value) {
-                $db->query('
-                    UPDATE xf_bdad_ad
-                    SET click_count = click_count + ?
-                    WHERE ad_id = ?
-                ', array($value, $varName));
+            function (Zend_Db_Adapter_Abstract $db, $values) {
+                self::_increaseDbColumnByKey($db, 'xf_bdad_ad', 'click_count', 'ad_id', $values);
             }
         );
     }
@@ -37,12 +29,8 @@ class bdCloudServerHelper_Helper_RedisValueSync
     {
         self::_start(
             'bdad_view',
-            function (Zend_Db_Adapter_Abstract $db, $varName, $value) {
-                $db->query('
-                    UPDATE xf_bdad_ad
-                    SET view_count = view_count + ?
-                    WHERE ad_id = ?
-                ', array($value, $varName));
+            function (Zend_Db_Adapter_Abstract $db, $values) {
+                self::_increaseDbColumnByKey($db, 'xf_bdad_ad', 'view_count', 'ad_id', $values);
             }
         );
     }
@@ -51,12 +39,8 @@ class bdCloudServerHelper_Helper_RedisValueSync
     {
         self::_start(
             'image_proxy_view',
-            function (Zend_Db_Adapter_Abstract $db, $varName, $value) {
-                $db->query('
-                    UPDATE xf_image_proxy
-                    SET views = views + ?
-                    WHERE image_id = ?
-                ', array($value, $varName));
+            function (Zend_Db_Adapter_Abstract $db, $values) {
+                self::_increaseDbColumnByKey($db, 'xf_image_proxy', 'views', 'image_id', $values);
             }
         );
     }
@@ -65,21 +49,28 @@ class bdCloudServerHelper_Helper_RedisValueSync
     {
         self::_start(
             'ip_login',
-            function (Zend_Db_Adapter_Abstract $db, $userId, $value) {
-                $value = @unserialize($value);
-                if (isset($value['ip'])
-                    && isset($value['log_date'])
-                    && $userId > 0
-                ) {
-                    $db->insert('xf_ip', array(
-                        'user_id' => $userId,
-                        'content_type' => 'user',
-                        'content_id' => $userId,
-                        'action' => 'login',
-                        'ip' => $value['ip'],
-                        'log_date' => $value['log_date'],
-                    ));
+            function (Zend_Db_Adapter_Abstract $db, $values) {
+                $queryValues = array();
+                foreach ($values as $userId => $value) {
+                    $value = @unserialize($value);
+                    if (isset($value['ip'])
+                        && isset($value['log_date'])
+                        && $userId > 0
+                    ) {
+                        $queryValues[] = sprintf('(%1$d, "user", %1$d, "login", %2$s, %3$d)',
+                            $userId, $db->quote($value['ip']), $value['log_date']);
+                    }
                 }
+
+                if (count($queryValues) === 0) {
+                    return;
+                }
+
+                $db->query('
+                    INSERT INTO xf_ip
+                    (user_id, content_type, content_id, action, ip, log_date)
+                    VALUES ' . implode(', ', $queryValues)
+                );
             }
         );
     }
@@ -88,14 +79,16 @@ class bdCloudServerHelper_Helper_RedisValueSync
     {
         self::_start(
             'session_activity',
-            function (Zend_Db_Adapter_Abstract $db, $userId, $timestamp) {
-                $db->update('xf_user',
-                    array('last_activity' => $timestamp),
-                    array(
-                        'user_id = ?' => $userId,
-                        'last_activity < ?' => $timestamp,
-                    )
-                );
+            function (Zend_Db_Adapter_Abstract $db, $values) {
+                foreach ($values as $userId => $timestamp) {
+                    $db->update('xf_user',
+                        array('last_activity' => $timestamp),
+                        array(
+                            'user_id = ?' => $userId,
+                            'last_activity < ?' => $timestamp,
+                        )
+                    );
+                }
             }
         );
     }
@@ -104,12 +97,8 @@ class bdCloudServerHelper_Helper_RedisValueSync
     {
         self::_start(
             'thread_view',
-            function (Zend_Db_Adapter_Abstract $db, $varName, $value) {
-                $db->query('
-                    UPDATE xf_thread
-                    SET view_count = view_count + ?
-                    WHERE thread_id = ?
-                ', array($value, $varName));
+            function (Zend_Db_Adapter_Abstract $db, $values) {
+                self::_increaseDbColumnByKey($db, 'xf_thread', 'view_count', 'thread_id', $values);
             }
         );
     }
@@ -133,38 +122,35 @@ class bdCloudServerHelper_Helper_RedisValueSync
         while (true) {
             $values = $redis->hScan($hashKey, $iterator, '', self::$_syncToDbBatch);
             if (empty($values)) {
-                if ($echo && $i > 0) {
+                if ($echo && $i === 0) {
                     echo(sprintf("[%s#%d] No values found\n", $type, $i));
                 }
                 break;
             }
 
-            $updatedVarNames = array();
-
-            foreach ($values as $varName => $value) {
-                try {
-                    $result = call_user_func($callback, $db, $varName, $value);
-                    if ($result === false) {
-                        $maxTime = 0;
-                        break;
-                    }
-                    $updatedVarNames[] = $varName;
-                } catch (Zend_Db_Exception $e) {
-                    XenForo_Error::logError($e->getMessage());
-
-                    if ($echo) {
-                        echo(sprintf("[%s#%d] Error %s", $type, $i, $e->getMessage()));
-                    }
-
+            try {
+                $result = call_user_func($callback, $db, $values);
+                if ($result === false) {
                     $maxTime = 0;
-                    break;
                 }
+            } catch (Zend_Db_Exception $e) {
+                XenForo_Error::logError($e->getMessage());
+
+                if ($echo) {
+                    echo(sprintf("[%s#%d] Error %s", $type, $i, $e->getMessage()));
+                }
+
+                $maxTime = 0;
             }
 
+            $updatedVarNames = array_keys($values);
             bdCloudServerHelper_Helper_Redis::deleteValues($type, $updatedVarNames);
 
-            if ($echo) {
-                echo(sprintf("[%s#%d] Updated %s\n", $type, $i, implode(' ', $updatedVarNames)));
+            if (XenForo_Application::debugMode()) {
+                XenForo_Helper_File::log(__CLASS__, sprintf("[%s#%d] Updated %s",
+                    $type, $i, implode(' ', $updatedVarNames)));
+            } elseif ($echo) {
+                echo(sprintf("[%s#%d] Updated %d\n", $type, $i, count($updatedVarNames)));
             }
 
             $elapsedTime = microtime(true) - $startTime;
@@ -180,8 +166,34 @@ class bdCloudServerHelper_Helper_RedisValueSync
             $i++;
         }
 
-        if ($echo && $i === 0) {
+        if ($echo) {
             echo(sprintf("[%s#%d] Done\n", $type, $i));
         }
+    }
+
+    protected static function _increaseDbColumnByKey(Zend_Db_Adapter_Abstract $db, $tbl, $col, $key, array $values)
+    {
+        if (count($values) === 0) {
+            return;
+        }
+
+        $cases = array();
+        foreach ($values as $keyValue => $colValue) {
+            $colValue = intval($colValue);
+            if ($colValue === 0) {
+                continue;
+            }
+
+            $cases[] = sprintf('WHEN %s = %s THEN %s', $key, $db->quote($keyValue), $colValue);
+        }
+
+        $q = sprintf('UPDATE %1$s SET %2$s = %2$s + CASE %4$s END WHERE %3$s IN (%5$s)',
+            $tbl, $col, $key, implode(' ', $cases), $db->quote(array_keys($values)));
+
+        if (XenForo_Application::debugMode()) {
+            XenForo_Helper_File::log(__CLASS__, $q);
+        }
+
+        $db->query($q);
     }
 }
